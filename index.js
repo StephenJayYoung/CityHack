3b83dc41886daf52b473fb54e16c5bbd6b7134d7
Stephen Young <StephenJayYoung@gmail.com>
2014-09-11 23:17:49 -0700

    added index with a test

---
+++ index.js
@@ -0,0 +1,22 @@
+ 
+
+ describe("Ers", function() {
+ 	// given an object, it looks for shared interests
+ 	it('looks for similarities between objects',  function() {
+ 	//reads the totalInterests file and looks for shared interests
+ 	var sharedInterests = require('..json');
+ 	expect(sharedInterests).to.eql(runner)
+  });
+
+var
+var 
+ });
+
+var ERs = {
+      "ER1": "runner",
+      "ER2": "climber",
+      "ER3": "musicMaker",
+      "ER4": "biker"
+      "ER5": "hiker",
+      "ER6": "beerDrinker"
+}
